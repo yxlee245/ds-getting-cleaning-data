93abed1367936f4b46677e007d61ee46dcd9c192
yxlee245 <41932856+yxlee245@users.noreply.github.com>
2020-02-06 23:06:36 +0800

    Added code to read in and preprocess feature names

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,34 @@
-### R script to clean Human Activity Recognition (HAR) data
+### R script to clean Human Activity Recognition (HAR) data into tidy format ###
 
-# Merge the training and the test sets to create one data set
+## Load libraries
+library(dplyr)
 
-# Extracts only the measurements on the mean and standard deviation for each measurement.
 
-# Uses descriptive activity names to name the activities in the data set
+## Merge the training and the test sets to create one data set
 
-# Appropriately labels the data set with descriptive variable names.
+# Read in raw names of features and put through preprocess pipes
+con <- file("./uci_har_dataset/features.txt")
+feature_names <-
+  con %>%
+  readLines %>%
+  # Split each feature name by space character
+  strsplit(" ") %>%
+  # Get second item in character vector in every list item
+  sapply(function(split_items) {split_items[2]})
 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Remove parentheses in feature names
+feature_names <- gsub("[()]", "", feature_names)
+close(con)
+
+
+## Extracts only the measurements on the mean and standard deviation for each measurement.
+
+
+## Uses descriptive activity names to name the activities in the data set
+
+
+## Appropriately labels the data set with descriptive variable names.
+
+
+## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
